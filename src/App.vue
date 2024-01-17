99471ab8c0692ff6530d10975fb03ca5c927f116
Giuseppe Failla <146582696+GiuseppeF11@users.noreply.github.com>
2024-01-17 15:11:54 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script>
+
+/* Per usare un componente dentro un altr:
+1) Importazione del componente 
+2) Dichiarazione del componente all'interno dell'oggetto
+3) Utilizzo del componente
+*/
+
+//1)
+import AppHeader from './components/AppHeader.vue';
+
+export default {
+  data() {
+    return { 
+      count: 0 
+    };
+  },
+  
+  //2) Dichiarazione dei componenti
+  components: {
+    AppHeader
+  }
+}
+</script>
+
+<template>
+  <div>
+    <!-- 3) Utilizzo del componente -->
+      <AppHeader/>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+  data() {
+    return { 
+      title: 'Hello World'
+    };
+  },
+};
+</script>
+
+<template>
+  <header>
+    <h1 class="main-title">
+      {{ title }}
+    </h1>
+  </header>
+</template>
+
+<style scoped>
+  .main-title {
+    background-color: aquamarine;
+    color: lightcoral;
+    text-align: center;
+  }
+
+</style>
